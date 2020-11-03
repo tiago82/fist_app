e06f5a17f194a2462e2fd9c2617359a2b59c98f0
Tiago <tiago8204@gmail.com>
2020-11-03 19:59:31 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/fist_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fist_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/views/MainView.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class MainView extends StatefulWidget {
+  @override
+  _MainViewState createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('a'),
+        SizedBox(
+          child: Row(
+            children: [
+              Text('aaadfsd'),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Row(children: [
+              Padding(
+                child: Text('Hello World!'),
+                padding: EdgeInsets.all(50.0),
+              ),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(),
+                ),
+              ),
+            ]))
+      ],
+    )));
+  }
+}
