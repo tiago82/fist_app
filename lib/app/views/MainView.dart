14be9df0835cd67e6a4e99127f58cc052fe821c2
Tiago <tiago8204@gmail.com>
2020-11-04 17:36:18 -0300

    daily

--- lib/app/views/MainView.dart
+++ lib/app/views/MainView.dart
@@ -9,37 +9,82 @@ class _MainViewState extends State<MainView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text('Porra'),
-        SizedBox(
-          child: Row(
-            children: [
-              Text('aaadfsd'),
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(),
-                ),
+      appBar: AppBar(
+        title: Text('App Name'),
+      ),
+      body: Container(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                width: 200.0,
+                height: 50.0,
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Numero1 :",
+                        style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w200,
+                            fontFamily: "Roboto"),
+                      ),
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(),
+                        ),
+                      ),
+                    ]),
               ),
-            ],
-          ),
-        ),
-        SizedBox(
-            width: MediaQuery.of(context).size.width,
-            child: Row(children: [
-              Padding(
-                child: Text('Hello World!'),
-                padding: EdgeInsets.all(50.0),
+              SizedBox(
+                width: 200.0,
+                height: 50.0,
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Numero2 :",
+                        style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w200,
+                            fontFamily: "Roboto"),
+                      ),
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(),
+                        ),
+                      ),
+                    ]),
               ),
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(),
-                ),
+              SizedBox(
+                width: 200.0,
+                height: 100.0,
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      RaisedButton(
+                        onPressed: () {},
+                        child: const Text('Enabled Button',
+                            style: TextStyle(fontSize: 20)),
+                      ),
+                    ]),
               ),
-            ]))
-      ],
-    )));
+            ]),
+        padding: const EdgeInsets.all(0.0),
+        alignment: Alignment.center,
+        width: 1.7976931348623157e+308,
+        height: 1.7976931348623157e+308,
+      ),
+    );
   }
+
+  void buttonPressed() {}
 }
